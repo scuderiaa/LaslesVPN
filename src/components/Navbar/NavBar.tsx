f50a1872ffde2288fd1f1952c81811105e1fe46f
Sammy Michael <nduwayosammymichael@gmail.com>
2025-04-01 08:05:52 +0200

    first commit

---
+++ src/components/Navbar/NavBar.tsx
@@ -0,0 +1,11 @@
+import logo from '../../assets/Logo.svg'
+
+export const NavBar = () => {
+    return (
+        <div>
+            <div className="flex items-center">
+                <img src={logo} alt="logo" className="" />
+            </div>
+        </div>
+    )
+}
